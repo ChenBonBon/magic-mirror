809c90139543ac63568bda04cbb050fb06f14e37
BonBon <per_cherry@163.com>
2024-12-07 23:29:39 +0800

    更新打印机接口

--- src/services/print.ts
+++ src/services/print.ts
@@ -1,10 +1,10 @@
-import http from "../http";
+import axios from "axios";
 import { HTTPResponse } from "../models/http";
 import { PrintRes } from "../models/print";
 
 export async function print(imgUrl: string) {
-  const res = await http.get<HTTPResponse<PrintRes>>(
-    "/image-print?imgUrl=" + imgUrl
+  const res = await axios.get<HTTPResponse<PrintRes>>(
+    "http://localhost:8081/api/v1/image-print?imgUrl=" + imgUrl
   );
 
   if (res.data && res.data.code === 200) {
